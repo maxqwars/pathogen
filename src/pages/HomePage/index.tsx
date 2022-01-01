7d4eeabf82378cd3df52eefb040251876c5d42bb
Maxim "maxqwars" Maximenko <maxqwars@gmail.com>
2022-01-02 04:19:37 +0700

    Changed: HomePage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -15,7 +15,18 @@
 // You should have received a copy of the GNU General Public License
 // along with @maxqwars/pathogen.  If not, see <http://www.gnu.org/licenses/>.
 
-import { IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react'
+import {
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react'
 
 import { AppNeedSetupCard } from '../../components'
 import { CENTERED_COLUMN } from '../../constants/CENTERED_COLUMN'
@@ -29,26 +40,46 @@ const HomePage = (props: IHomePageProps) => {
   const apiUrl = useAppSelector(state => state.appConfig.apiUrl)
   const { t } = useTranslation()
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>{t('home-page-toolbar-label')}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+  const page = (content: React.ReactNode) => {
+    return (
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">{t('home-page-toolbar-label')}</IonTitle>
+            <IonTitle>{t('home-page-toolbar-label')}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonGrid>
-          <IonRow>
-            <IonCol {...CENTERED_COLUMN}>{apiUrl?.length === 0 ? <AppNeedSetupCard /> : null}</IonCol>
-          </IonRow>
-        </IonGrid>
-      </IonContent>
-    </IonPage>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">{t('home-page-toolbar-label')}</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          {content}
+        </IonContent>
+      </IonPage>
+    )
+  }
+
+  if (apiUrl?.length === 0) {
+    return page(
+      <IonGrid>
+        <IonRow>
+          <IonCol {...CENTERED_COLUMN}>{apiUrl?.length === 0 ? <AppNeedSetupCard /> : null}</IonCol>
+        </IonRow>
+      </IonGrid>
+    )
+  }
+
+  return page(
+    <IonGrid>
+      <IonRow>
+        <IonCol {...CENTERED_COLUMN}>
+          <IonCard>
+            <IonCardContent>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, perferendis.</IonCardContent>
+          </IonCard>
+        </IonCol>
+      </IonRow>
+    </IonGrid>
   )
 }
 
