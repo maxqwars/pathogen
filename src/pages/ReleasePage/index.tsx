757adb01ca6ba7b1391d12108e5f87bcd5b2dc75
Maxim Maximenko <maxqwars@gmail.com>
2022-01-15 03:03:59 +0700

    Added: localization

--- src/pages/ReleasePage/index.tsx
+++ src/pages/ReleasePage/index.tsx
@@ -15,9 +15,19 @@
 // You should have received a copy of the GNU General Public License
 // along with @maxqwars/pathogen.  If not, see <http://www.gnu.org/licenses/>.
 
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonProgressBar } from '@ionic/react'
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonProgressBar,
+  IonBackButton,
+  IonButtons
+} from '@ionic/react'
 import { DatabaseTypes } from '@maxqwars/xconn'
 import React, { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router'
 import { ReleaseView } from '../../containers'
 import { useAppSelector } from '../../redux/hooks'
@@ -28,6 +38,7 @@ interface IReleasePageProps {}
 const ReleasePage = (props: IReleasePageProps) => {
   const [release, setRelease] = useState<DatabaseTypes.ITitle | null>(null)
   const apiUrl = useAppSelector(state => state.appConfig.apiUrl)
+  const { t } = useTranslation()
   const params = useParams<{ code: string }>()
 
   useEffect(() => {
@@ -50,21 +61,15 @@ const ReleasePage = (props: IReleasePageProps) => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Release_Page</IonTitle>
+          <IonButtons slot="start">
+            <IonBackButton />
+          </IonButtons>
+          <IonTitle>{t('release-page-header-label')}</IonTitle>
         </IonToolbar>
         {release === null ? <IonProgressBar type="indeterminate" /> : null}
       </IonHeader>
 
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle>Release_Page</IonTitle>
-          </IonToolbar>
-          {release === null ? <IonProgressBar type="indeterminate" /> : null}
-        </IonHeader>
-
-        {release !== null ? <ReleaseView release={release} /> : null}
-      </IonContent>
+      <IonContent fullscreen>{release !== null ? <ReleaseView release={release} /> : null}</IonContent>
     </IonPage>
   )
 }
