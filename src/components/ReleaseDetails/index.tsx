e777580906e6b4a8fdd9d038f6b7091f0478be2d
Maxim Maximenko <maxqwars@gmail.com>
2022-01-21 14:04:39 +0700

    added: extend russian translate, changed: ReleaseDetails

--- src/components/ReleaseDetails/index.tsx
+++ src/components/ReleaseDetails/index.tsx
@@ -15,8 +15,11 @@
 // You should have received a copy of the GNU General Public License
 // along with @maxqwars/pathogen.  If not, see <http://www.gnu.org/licenses/>.
 
-import { DatabaseTypes } from '@maxqwars/xconn'
+import { IonCard, IonCardHeader, IonCardTitle } from '@ionic/react'
+import { DatabaseTypes, TITLE_CONTENT_TYPE_CODE_ENUM, TITLE_SEASON_CODE_ENUM } from '@maxqwars/xconn'
+import { useTranslation } from 'react-i18next'
 import styles from './index.module.css'
+import millify from 'millify'
 
 interface IReleaseDetailsProps {
   release: DatabaseTypes.ITitle
@@ -24,9 +27,93 @@ interface IReleaseDetailsProps {
 
 const ReleaseDetails = (props: IReleaseDetailsProps) => {
   const { release } = props
+  const { t, i18n } = useTranslation()
+  const lng = i18n.language
+
+  console.log(release)
+
+  const decodeSeason = (seasonCode: TITLE_SEASON_CODE_ENUM) => {
+    switch (+seasonCode) {
+      default: {
+        return 'N/D'
+      }
+      case TITLE_SEASON_CODE_ENUM.AUTUMN: {
+        return t('season-autumn-label')
+      }
+      case TITLE_SEASON_CODE_ENUM.SPRING: {
+        return t('season-spring-label')
+      }
+      case TITLE_SEASON_CODE_ENUM.SUMMER: {
+        return t('season-summer-label')
+      }
+      case TITLE_SEASON_CODE_ENUM.WINTER: {
+        return t('season-winter-label')
+      }
+    }
+  }
+
+  const decodeType = (typeCode: TITLE_CONTENT_TYPE_CODE_ENUM) => {
+    switch (+typeCode) {
+      default: {
+        return 'N/D'
+      }
+      case TITLE_CONTENT_TYPE_CODE_ENUM.MOVIE: {
+        return 'MOVIE'
+      }
+      case TITLE_CONTENT_TYPE_CODE_ENUM.ONA: {
+        return 'ONA'
+      }
+      case TITLE_CONTENT_TYPE_CODE_ENUM.OVA: {
+        return 'OVA'
+      }
+      case TITLE_CONTENT_TYPE_CODE_ENUM.SPECIAL: {
+        return 'SPECIAL'
+      }
+      case TITLE_CONTENT_TYPE_CODE_ENUM.TV: {
+        return 'TV'
+      }
+    }
+  }
 
   return (
     <div className={styles['release-details']}>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>{lng === 'ru' ? release?.names?.ru : release?.names?.en}</IonCardTitle>
+        </IonCardHeader>
+      </IonCard>
+
+      <div className={styles['release-details__stats-box']}>
+        <div className={styles['release-details__stat-item']}>
+          <div className={styles['release-details__stat-item-value']}>{millify(release.inFavorites as number)}</div>
+          <div className={styles['release-details__stat_item-sign']}>{t('saves-label').toLowerCase()}</div>
+        </div>
+
+        <div className={styles['release-details__stat-item']}>
+          <div className={styles['release-details__stat-item-value']}>
+            {decodeType(release.type?.code as TITLE_CONTENT_TYPE_CODE_ENUM)}
+          </div>
+          <div className={styles['release-details__stat_item-sign']}>{t('type-label').toLowerCase()}</div>
+        </div>
+
+        <div className={styles['release-details__stat-item']}>
+          <div className={styles['release-details__stat-item-value']}>{release?.type?.series}</div>
+          <div className={styles['release-details__stat_item-sign']}>{t('episodes-label').toLowerCase()}</div>
+        </div>
+
+        <div className={styles['release-details__stat-item']}>
+          <div className={styles['release-details__stat-item-value']}>{release?.season?.year}</div>
+          <div className={styles['release-details__stat_item-sign']}>{t('year-label').toLowerCase()}</div>
+        </div>
+
+        <div className={styles['release-details__stat-item']}>
+          <div className={styles['release-details__stat-item-value']}>
+            {decodeSeason(release?.season?.code as TITLE_SEASON_CODE_ENUM)}
+          </div>
+          <div className={styles['release-details__stat_item-sign']}>{t('season-label').toLowerCase()}</div>
+        </div>
+      </div>
+
       <div className={styles['release-details__box']}>{release.description}</div>
     </div>
   )
